c041a9bffb36146bc4d8b563ca5330ee2545c8dd
mlambotaku9 <140350786+mlambotaku9@users.noreply.github.com>
2024-01-07 00:14:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -63,29 +63,28 @@ global.message = {
 },
 
 module.exports = {
-  botname: process.env.BotName || "⇶ 𝔏𝔦𝔰à𝔥 ❁", 
+  botname: process.env.BotName || "Queen Anya", 
   author: process.env.Author || "@PikaBotz",
   packname: process.env.PackName || "Queen Anya v2 MD",
   socialLink: process.env.Web || "https://github.com/PikaBotz",
-  footer: process.env.Footer || "© ⇶ 𝔏𝔦𝔰à𝔥 ❁",
+  footer: process.env.Footer || "© Queen Anya Bot",
   prefa: process.env.Prefix || ['-'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "⇶ ℜ𝔦𝔨𝔦 𝔫𝔢𝔫𝔡𝔬𝔲 ❁",
-  ownernumber: process.env.Owner_Number || "263775571820,263714406565",
-  instagramId: process.env.Insta || "tkmtaku1",
+  ownername: process.env.Owner_Name || "Pika~Kun",
+  ownernumber: process.env.Owner_Number || "916900904828",
+  instagramId: process.env.Insta || "8.08_only_mine",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "mongoUrl: process.env.MongoDB || "mongodb+srv://DARKSHAN:3000@cluster0.t1wsjlv.mongodb.net/",
-  welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',",
+  mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "ZjgzRkd_Queen-Anya_VNWU=", 
-  image_1: readFileSync('./lib/Assets/IMG-20240101-WA0132.jpg'), // Thumbnail for allmenu command
-  image_2: readFileSync('./lib/Assets/IMG-20240101-WA0132.jpg'), // null image
-  image_3: readFileSync("./lib/Assets/IMG-20240101-WA0132.jpg"), // Thumbnail for Dashboard
-  aliveMedia: readFileSync("./lib/Assets/IMG-20240101-WA0132.jpg"),
-  menuMedia: readFileSync('./lib/Assets/IMG-20240101-WA0132.jpg'),
+  sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
+  image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
+  image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
+  image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
+  aliveMedia: readFileSync("./lib/Assets/aliveMedia.mp4"),
+  menuMedia: readFileSync('./lib/Assets/menuMedia.mp4'),
   badWords: badWords,
   message: {
     success: message.success,
@@ -106,19 +105,19 @@ module.exports = {
 
 
 // Ignore them 👇🏻
-global.botname = process.env.BotName || "⇶ 𝔏𝔦𝔰à𝔥 ❁" 
+global.botname = process.env.BotName || "Queen Anya" 
 global.author = process.env.Author || "@PikaBotz" 
 global.packname = process.env.PackName || "Queen Anya v2 MD" 
 global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© ⇶ 𝔏𝔦𝔰à𝔥 ❁" 
+global.footer = process.env.Footer || "© Queen Anya Bot" 
 global.prefa = process.env.Prefix || ['-'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "⇶ ℜ𝔦𝔨𝔦 𝔫𝔢𝔫𝔡𝔬𝔲 ❁" 
-global.ownernumber = process.env.Owner_Number || "263775571820,263714406565" 
+global.ownername = process.env.Owner_Name || "Pika~Kun" 
+global.ownernumber = process.env.Owner_Number || "916900904828" 
 global.adress = process.env.Continent || "Asia, India, Assam" 
 global.timezone = process.env.TimeZone || "Asia/Kolkata" 
-global.instagramId = process.env.Insta || "tkmtaku1" 
-global.email = process.env.Email_Id || "mlambotaku9@gmail.com" 
+global.instagramId = process.env.Insta || "8.08_only_mine" 
+global.email = process.env.Email_Id || "example@example.com" 
   
 //--------------- Tip ----------------\\
 global.Tips = [
@@ -128,8 +127,7 @@ global.Tips = [
 ]
 
 //--------------- Menu images ----------------\\
-global.image_1 = readFileSync('./lib/Assets/IMG-20240101-WA0132.jpg') // Thumbnail for allmenu command
-global.image_2 = readFileSync('./lib/Assets/IMG-20240101-WA0132.jpg') // null image
-global.image_3 = readFileSync("./lib/Assets/IMG-20240101-WA0132.jpg") // Thumbnail for Dashboard
+global.image_1 = readFileSync('./lib/Assets/image_1.jpg') // Thumbnail for allmenu command
+global.image_2 = readFileSync('./lib/Assets/image_2.jpg') // null image
+global.image_3 = readFileSync("./lib/Assets/image_3.jpg") // Thumbnail for Dashboard
 global.menu_pic = "https://i.ibb.co/PhDcZTM/Thumbnail.png";
-
